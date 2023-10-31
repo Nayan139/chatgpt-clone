dd986ee2e06ff48a02fed62a57672c574e4df300
nyn <nayanbhakhar7075@gmail.com>
2023-10-31 18:50:54 +0530

    Firebase config update

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB9bUT9-hVCFcGsEsBLBbBGjsT7BPRjfxk",
-  authDomain: "chatgpt-yt-c2e64.firebaseapp.com",
-  projectId: "chatgpt-yt-c2e64",
-  storageBucket: "chatgpt-yt-c2e64.appspot.com",
-  messagingSenderId: "271651098660",
-  appId: "1:271651098660:web:bd80e0e27049531fdec8b1",
-  measurementId: "G-6Q512K2P0V",
+  apiKey: process.env.APIKEY!,
+  authDomain: process.env.AUTHDOMAIN!,
+  projectId: process.env.PROJECTID!,
+  storageBucket: process.env.STORAGEBUCKET!,
+  messagingSenderId: process.env.MESSAGINGSENDERID!,
+  appId: process.env.APPID!,
+  measurementId: process.env.MEASUREMENTID!,
 };
 
 // Initialize Firebase
