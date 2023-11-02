2fadc60eb5eace8bfe12e72a9580c0ad91d72695
nyn <nayanbhakhar7075@gmail.com>
2023-11-02 18:27:29 +0530

    package installation

---
+++ typing.d.ts
@@ -0,0 +1,9 @@
+interface Message {
+  text: string;
+  createdAt: admin.firestore.Timestamp;
+  user: {
+    _id: string | null | undefined;
+    name: string | null | undefined;
+    avatar: string | null | undefined;
+  };
+}
